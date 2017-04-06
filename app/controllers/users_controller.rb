7fa1769d1181ef46be7c3c5c0983714290d2b823
Tom Quinonero <tom@tomtomtom.fr>
2017-04-06 17:35:06 +0200

    handle file upload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,11 @@ class UsersController < ApplicationController
   def add_users
     if !users_params[:csv_raw].blank?
       response = Faraday.post 'http://localhost:5000/csv_processing', { :data => users_params[:csv_raw] } 
-
       handle_api_call(response)
-
     elsif !users_params[:csv_file].blank?
-      puts 'file'
+      up_file = users_params[:csv_file]
+      response = Faraday.post 'http://localhost:5000/csv_processing', { :data => up_file.read } 
+      handle_api_call(response)
     else
       redirect_to add_users_path, alert: "You have to specify a text or a file"
     end
